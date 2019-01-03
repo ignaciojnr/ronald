303399d6655371ab2ecfa112929387b56fa936ba
ignaciojnr <38479182+ignaciojnr@users.noreply.github.com>
2019-01-03 17:20:12 -0300

    form config

--- ronald/configurarConeccion.cs
+++ ronald/configurarConeccion.cs
@@ -16,5 +16,26 @@ namespace ronald
         {
             InitializeComponent();
         }
+
+        private void configurarConeccion_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = cosasGlobales.servidor;
+            textBox2.Text = cosasGlobales.baseDeDatos;
+            textBox3.Text = cosasGlobales.usuario;
+            textBox5.Text = cosasGlobales.SSLmode;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!cosasGlobales.reConectar(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text))
+                cosasGlobales.mensajeError("No se pudo establecer la conexión con la base de datos.");
+            else
+                this.Close();
+        }
     }
 }
